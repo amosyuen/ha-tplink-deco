a37dfff1fb35854aa447796eb5661bcde37b8b43
az <102905929+az01101001@users.noreply.github.com>
2023-07-30 23:52:29 +0800

    Fix SSLV3_ALERT_HANDSHAKE_FAILURE for Deco X20 (#210)
    Use ssl context instead of verify_ssl (#209)

--- custom_components/tplink_deco/api.py
+++ custom_components/tplink_deco/api.py
@@ -6,6 +6,7 @@ import logging
 import math
 import re
 import secrets
+import ssl
 from typing import Any
 from urllib.parse import quote_plus
 
@@ -131,7 +132,6 @@ class TplinkDecoApi:
         self._host = host
         self._username = username
         self._password = password
-        self._verify_ssl = verify_ssl
         self._session = session
         self._timeout_error_retries = timeout_error_retries
         self._timeout_seconds = timeout_seconds
@@ -151,6 +151,15 @@ class TplinkDecoApi:
         self._seq = None
         self._stok = None
         self._cookie = None
+
+        if verify_ssl:
+            self._ssl_context = None
+        else:
+            context = ssl.create_default_context()
+            context.set_ciphers("DEFAULT")
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
+            self._ssl_context = context
 
     # Return list of deco devices
     async def async_list_devices(self) -> dict:
@@ -402,7 +411,7 @@ class TplinkDecoApi:
                     params=params,
                     data=data,
                     headers=headers,
-                    verify_ssl=self._verify_ssl,
+                    ssl=self._ssl_context,
                 )
                 response.raise_for_status()
 
