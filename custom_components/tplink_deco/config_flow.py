db958a13b61b06ae877f17469dba9a3146003d45
Darcy <51043706+dtbrown01@users.noreply.github.com>
2026-01-21 06:19:58 +1100

    Fix removal of config_entry setter in 2025.12 (#459)

--- custom_components/tplink_deco/config_flow.py
+++ custom_components/tplink_deco/config_flow.py
@@ -210,7 +210,6 @@ class TplinkDecoOptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: ConfigEntry):
         """Initialize HACS options flow."""
-        self.config_entry = config_entry
         self.data = dict(config_entry.data)
         self._errors = {}
 
